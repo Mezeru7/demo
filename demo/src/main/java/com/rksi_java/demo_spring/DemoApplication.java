943214591307dd513d13891ce2ea4cf2533e9030
Mezeru7 <okolelov.ivan@gmail.com>
2024-12-17 17:23:22 +0300

    первый коммит

---
+++ demo/src/main/java/com/rksi_java/demo_spring/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.rksi_java.demo_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
